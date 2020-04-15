0d45493e82f2561fa4cdd2dab4ae87f93a66ccc7
Radek <radek@MacBook-Pro-Radek.local>
2020-04-15 15:20:27 +0200

    Updates

---
+++ precache-manifest.6627d1ebab7c13a950f5e3c458fca328.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f5e59ad452c649913987d58a585b7c",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "485f801cbaffe486f639",
+    "url": "/chat/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "f2db5af8afb50305114c",
+    "url": "/chat/static/js/2.0d81de69.chunk.js"
+  },
+  {
+    "revision": "485f801cbaffe486f639",
+    "url": "/chat/static/js/main.e3cbfeed.chunk.js"
+  },
+  {
+    "revision": "5bb637843dac5d879ad5",
+    "url": "/chat/static/js/runtime~main.54cd968c.js"
+  }
+]);
